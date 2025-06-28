3207dc90ba0bf66624f6db2a4259940145cbc413
仲優樹 <yu-ki216m@outlook.jp>
2025-06-28 15:55:27 +0900

    Initial Commit

---
+++ TaskManagementSwiftConcurrency/TaskManagementSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementSwiftConcurrencyApp.swift
+//  TaskManagementSwiftConcurrency
+//
+//  Created by 仲優樹 on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
